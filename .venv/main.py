f48f3b4153cfc409cb756f93b3eaac3548537588
sergey <savchenko.sergey.v@gmail.com>
2024-01-02 20:39:16 +0200

    first commin new games....

--- .venv/main.py
+++ .venv/main.py
@@ -1,15 +1,22 @@
 import pygame
+from pygame.constants import QUIT
 
 pygame.init()
 
-HEIGHT=1200
-WIDTH=800
+HEIGHT = 600 #800
+WIDTH = 800 #1200
 
-main_display=pygame.display.set_mode((HEIGHT, WIDTH))
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-count =0
-while True:
-    print("")
-    count+=1
-    if count>=100:
-        break
+player = pygame.Surface((20, 20))
+player.fill((255, 255, 255))
+
+plaing = True
+while plaing:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            plaing = False
+
+    main_display.blit(player, (0, 0))
+
+    pygame.display.flip()
