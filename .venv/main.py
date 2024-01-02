cee0778a6c1c62c097ad40e480b779de0103682b
syteser <savchenko.sergey.v@gmail.com>
2024-01-02 21:12:26 +0200

    small fix

--- .venv/main.py
+++ .venv/main.py
@@ -5,11 +5,11 @@ from pygame.constants import QUIT
 HEIGHT = 600  # 800
 WIDTH = 800  # 1200
 COLOR_WHITE = (255, 255, 255)
-COLOR_BLACK=(0,0,0)
+COLOR_BLACK = (0, 0, 0)
 PLAYER_SIZE = (20, 20)
 
 pygame.init()
-FPS=pygame.time.Clock()
+FPS = pygame.time.Clock()
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
 player = pygame.Surface(PLAYER_SIZE)
@@ -26,12 +26,12 @@ while plaing:
 
     main_display.fill((COLOR_BLACK))
 
-    if player_rect.bottom>=HEIGHT or player_rect.top<0:
-        player_speed[1]=-player_speed[1]
-    if player_rect.right>=WIDTH or player_rect.left<0:
-        player_speed[0]=-player_speed[0]
+    if player_rect.bottom >= HEIGHT or player_rect.top < 0:
+        player_speed[1] = -player_speed[1]
+    if player_rect.right >= WIDTH or player_rect.left < 0:
+        player_speed[0] = -player_speed[0]
 
     '''рисуем игрока, смещаем игрока, обновляем экран'''
     main_display.blit(player, player_rect)
-    player_rect=player_rect.move(player_speed)
+    player_rect = player_rect.move(player_speed)
     pygame.display.flip()
