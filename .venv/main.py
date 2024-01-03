0eb51f1f5121bf9e880d6fcfe7652800e6082e22
sergey <savchenko.sergey.v@gmail.com>
2024-01-03 19:23:47 +0200

    жвижение стрелками вверх/вниз/вправо/влево

--- .venv/main.py
+++ .venv/main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 '''константы'''
 HEIGHT = 600  # 800
@@ -17,7 +17,12 @@ main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 player = pygame.Surface(PLAYER_SIZE)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+# player_speed = [1, 1]
+player_move_down = [0, 1]
+player_move_up = [0, -1]
+player_move_right=[1,0]
+player_move_left=[-1,0]
+
 
 plaing = True
 while plaing:
@@ -28,20 +33,31 @@ while plaing:
 
     main_display.fill((COLOR_BLACK))
 
+    keys = pygame.key.get_pressed()
+
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+    if keys[K_UP] and player_rect.top >0:
+        player_rect=player_rect.move(player_move_up)
+    if keys[K_RIGHT] and player_rect.right <WIDTH:
+        player_rect=player_rect.move(player_move_right)
+    if keys[K_LEFT] and player_rect.left >0:
+        player_rect=player_rect.move(player_move_left)
+
+    '''рисуем игрока, смещаем игрока, обновляем экран'''
+    main_display.blit(player, player_rect)
+    #    player_rect = player_rect.move(player_speed)
+    pygame.display.flip()
+
     #   if player_rect.bottom >= HEIGHT or player_rect.top < 0:
     #       player_speed[1] = -player_speed[1]
     #   if player_rect.right >= WIDTH or player_rect.left < 0:
     #       player_speed[0] = -player_speed[0]
-    if player_rect.bottom >= HEIGHT:
-        player_speed = random.choice(([1, -1], [-1, -1]))
-    if player_rect.top <= 0:
-        player_speed = random.choice(([-1, 1], [1, 1]))
-    if player_rect.right >= WIDTH:
-        player_speed = random.choice(([-1, -1], [-1, 1]))
-    if player_rect.left <= 0:
-        player_speed = random.choice(([1, 1], [1, -1]))
-
-    '''рисуем игрока, смещаем игрока, обновляем экран'''
-    main_display.blit(player, player_rect)
-    player_rect = player_rect.move(player_speed)
-    pygame.display.flip()
+#    if player_rect.bottom >= HEIGHT:
+#        player_speed = random.choice(([1, -1], [-1, -1]))
+#    if player_rect.top <= 0:
+#        player_speed = random.choice(([-1, 1], [1, 1]))
+#    if player_rect.right >= WIDTH:
+#        player_speed = random.choice(([-1, -1], [-1, 1]))
+#    if player_rect.left <= 0:
+#        player_speed = random.choice(([1, 1], [1, -1]))
